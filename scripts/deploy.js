18f10b41515ad6b00604c25134f6aca2a16b5226
aleksey <aleksey.saraev.97@mail.ru>
2023-02-19 03:28:55 +0500

    маркетплейс deploy

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,19 +1,28 @@
 const hre = require("hardhat");
 
 async function main() {
-  const mainContractFactory = await hre.ethers.getContractFactory("Main")
-  mainContract = await mainContractFactory.deploy()
-  await mainContract.deployed()
+  const mainContractFactory = await hre.ethers.getContractFactory("Main");
+  mainContract = await mainContractFactory.deploy();
+  await mainContract.deployed();
 
   const mainAddress = await mainContract.address;
   const ownerAddress = await mainContract.getAddressContentOwner();
   const editorAddress = await mainContract.getAddressContentEditor();
   const viewerAddress = await mainContract.getAddressContentViewer();
 
+  const marketContractFactory = await hre.ethers.getContractFactory("MarketAccessContent");
+  marketContract = await marketContractFactory.deploy(ownerAddress, editorAddress, viewerAddress);
+  await marketContract.deployed();
+
+  const marketAddress = await marketContract.address;
+
+  await mainContract.setAddressMarket(marketAddress);
+
   console.log('main: ' + mainAddress);
   console.log('owner: ' + ownerAddress);
   console.log('editor: ' + editorAddress);
   console.log('viewer: ' + viewerAddress);
+  console.log('market: ' + marketAddress);
 
   await run('verify:verify', {
     address: mainAddress,
@@ -33,6 +42,11 @@ async function main() {
     address: viewerAddress,
     constructorArguments: [mainAddress, ownerAddress],
     contract: 'contracts/ContentViewer.sol:ContentViewer'
+  });
+  await run('verify:verify', {
+    address: marketAddress,
+    constructorArguments: [ownerAddress, editorAddress, viewerAddress],
+    contract: 'contracts/MarketAccessContent.sol:MarketAccessContent'
   });
 }
 
